0485c1c405b93508faadce840224b7e91a0a96cb
CarrilloAnguloisaac <143573719+CarrilloAnguloisaac@users.noreply.github.com>
2023-09-07 14:26:16 -0600

    Add files via upload

---
+++ _011_21110358_eliminar_elementos_de_lista.py
@@ -0,0 +1,5 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores)
+print("Eliminaremos el elemento amarillo")
+del colores[3]
+print(colores)
