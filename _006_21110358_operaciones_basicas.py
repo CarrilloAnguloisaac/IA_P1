a683fa4ff5892f6e77039626b3d26d4d51116da7
CarrilloAnguloisaac <143573719+CarrilloAnguloisaac@users.noreply.github.com>
2023-09-07 13:57:36 -0600

    Add files via upload

---
+++ _006_21110358_operaciones_basicas.py
@@ -0,0 +1,10 @@
+suma = 10 + 5 + 7 + 3 + 5
+print("Resultado: " , suma)
+resta= 300 - 7 - 23 - 89
+print("Resultado: " , resta)
+multi= 23*56*2
+print("Resultado: " , multi)
+div= 234/23
+print("Resultado: " , div)
+compleja= 23 + 45 - 45 * 34 / 2
+print("Resultado: " , compleja)
