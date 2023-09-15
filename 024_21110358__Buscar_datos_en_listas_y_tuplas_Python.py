2cc957d0eea9328b8f90c6b1481fc7532041825a
CarrilloAnguloisaac <143573719+CarrilloAnguloisaac@users.noreply.github.com>
2023-09-15 14:21:26 -0600

    Update and rename _024_21110358__Buscar_datos_en_listas_y_tuplas_Python.py to 024_21110358__Buscar_datos_en_listas_y_tuplas_Python.py

--- 024_21110358__Buscar_datos_en_listas_y_tuplas_Python.py
+++ 024_21110358__Buscar_datos_en_listas_y_tuplas_Python.py
@@ -2,12 +2,12 @@ colores = input('Introduce un color:\n')
 tupla_colores = ('rojo', 'azul', 'verde', 'amarillo')
 
 if colores in tupla_colores[0]:
-	print('El color rojo est· admitido')
+	print('El color rojo est√° admitido')
 elif colores in tupla_colores[1]:
-	print('El color azul est· admitido')
+	print('El color azul est√° admitido')
 elif colores in tupla_colores[2]:
-	print('El color verde est· admitido')
+	print('El color verde est√° admitido')
 elif colores in tupla_colores[3]:
-	print('El color amarillo est· admitido')
+	print('El color amarillo est√° admitido')
 else:
 	print('Color no admitido')
