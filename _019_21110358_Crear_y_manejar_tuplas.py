aa66cc7417867e21e86ddbe70680d85e21b575d3
CarrilloAnguloisaac <143573719+CarrilloAnguloisaac@users.noreply.github.com>
2023-09-15 13:55:27 -0600

    Add files via upload

---
+++ _019_21110358_Crear_y_manejar_tuplas.py
@@ -0,0 +1,5 @@
+tupla = ('rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(tupla[2])
+
+tupla = (10, 1, 5, 11)
+print( tupla[0] + tupla[0] + tupla[2])
