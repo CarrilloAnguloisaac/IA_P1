f78f81c015faa6db5b07652f4a8001432b7f1f20
CarrilloAnguloisaac <143573719+CarrilloAnguloisaac@users.noreply.github.com>
2023-09-15 14:05:26 -0600

    Add files via upload

---
+++ _021_21110358__El_condicional_IF.py
@@ -0,0 +1,16 @@
+num1 = 10
+num2 = 20
+
+if num1 > num2:
+	print('Se ejecuta el if mayor que.')
+if num1 < num2:
+	print('Se ejecuta el if menor que.')
+if num1 >= num2:
+	print('Se ejecuta el if mayor igual que.')
+if num1 <= num2:
+	print('Se ejecuta el if menor igual que.')
+if num1 == num2:
+	print('Se ejecuta el if igual que.')
+if num1 != num2:
+	print('Se ejecuta el if diferente que.')
+
