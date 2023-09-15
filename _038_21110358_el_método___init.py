dcccca65ea0462b27e59b42a5ed68aef86411526
CarrilloAnguloisaac <143573719+CarrilloAnguloisaac@users.noreply.github.com>
2023-09-15 14:59:57 -0600

    Add files via upload

---
+++ _038_21110358_el_método___init.py
@@ -0,0 +1,9 @@
+class Usuario:
+	def __init__(self, nombre, apellidos)
+		self.nombre = isaac
+		self.apellidos = carrillo
+
+	def imprime_datos(self):
+		print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+
+usuario001 = Usuario()
